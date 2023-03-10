23fcb3ee10880323ad68a41f44b7c11da1518c22
Indevor03 <124826427+Indevor03@users.noreply.github.com>
2023-03-10 20:46:51 +0300

    Заполнение задания 2

--- Task 02/Program.cs
+++ Task 02/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// 1. На вход принимает два числа
+// 2. Определяет наибольшее из них
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Ввод a");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввод b");
+int numberb = Convert.ToInt32(Console.ReadLine());
+if (numbera < numberb)
+{
+    Console.WriteLine("Наибольшее число b, наименьшее число a");
+}
+else
+{
+    Console.WriteLine("Наибольшее число а, нименьшее число b");
+}
